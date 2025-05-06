89a1683bedfe4b34cd2be476154c5bcf2c5b7bbb
julio <teste@email.com>
2025-05-06 09:43:59 -0300

    todas as funcionalidades implementadas ao codig

---
+++ bancodados.py
@@ -0,0 +1,9 @@
+import mysql.connector as mysql
+
+conexao = mysql.connect(
+
+    host = "127.0.0.1",
+    user = "root",
+    password = "julio",
+    database = "teste"
+)
--- index.py
+++ index.py
@@ -1,13 +1,37 @@
-import _mysql_connector as mysql
-import tkinter as tk
+# import mysql.connector as mysql
 
-conexao = mysql(
-    host = "localhost",
-    user = "root",
-    password = "teste",
-    database = "julio"
-)
+# conexao = mysql.connect(
 
-nome = input("Digite seu nome: ")
-altura = input("Digite sua altura: ")
+#     host = "127.0.0.1",
+#     user = "root",
+#     password = "julio",
+#     database = "teste"
+# )
 
+from bancodados import conexao
+
+cursor = conexao.cursor()
+
+def obterdado():
+
+    todos_os_valoeres = []
+
+    while True:
+
+        nome = input("Qual é o seu nome: ")
+
+        if nome == "sair":
+            break
+        else:
+            altura = int(input("Qual é a sua altura em centimetros: "))
+            valores = [nome, altura]
+            todos_os_valoeres.append(valores)
+
+            query = "INSERT INTO altura (nome, altura) VALUES (%s, %s)"
+            cursor.execute(query, valores)
+            conexao.commit()
+
+            print(f"{cursor.rowcount} registros inseridos.")
+
+while conexao.is_connected:
+    obterdado()
