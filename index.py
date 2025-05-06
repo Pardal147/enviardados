d18f844bca2fa0b6b6a5d29d80430d5fa91a7f99
julio <teste@email.com>
2025-05-05 21:16:34 -0300

    conexao ao banco de dados teste + inputs nome e altura adicionados

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
-print('teste')
+import _mysql_connector as mysql
+import tkinter as tk
+
+conexao = mysql(
+    host = "localhost",
+    user = "root",
+    password = "teste",
+    database = "julio"
+)
+
+nome = input("Digite seu nome: ")
+altura = input("Digite sua altura: ")
+
